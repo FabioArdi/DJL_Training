8c1792ab9e2af779196e5d80e77de861272af8c7
FabioArdi <arditfab@students.zhaw.ch>
2023-06-03 23:17:44 +0200

    first commit

---
+++ djl_training/src/main/java/ch/arditfab/djl_training/DjlTraining.java
@@ -0,0 +1,10 @@
+package ch.arditfab.djl_training;
+
+import java.io.IOException;
+import ai.djl.TranslateException;
+
+public class DjlTraining {
+
+    public static void main(String[] args) throws IOException, TranslateException {
+    
+}
---
+++ djl_training/src/main/java/ch/arditfab/djl_training/DjlTrainingApplication.java
@@ -0,0 +1,13 @@
+package ch.arditfab.djl_training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjlTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DjlTrainingApplication.class, args);
+	}
+
+}
